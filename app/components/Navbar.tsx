245a8ec96ab4d57f3bbac63b421493cc9a58db4a
Hudson Vo <hudson.m.vo@gmail.com>
2026-07-27 23:08:09 -0600

    added landing page

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav className="flex items-center justify-between px-6 py-4">
+      <Link href="/" className="font-semibold">
+        {/* TODO: wordmark/logo */}
+        Re-Chord
+      </Link>
+
+      <div className="flex items-center gap-4">
+        {/* TODO: real routes + styling once auth exists */}
+        <Link href="/browse">Browse</Link>
+        <Link href="/login">Sign in</Link>
+        <Link href="/signup">Sign up</Link>
+      </div>
+    </nav>
+  );
+}
