bdcd99cb7a6f1e906c41670d71f80c6fd14dc2ee
Hudson Vo <hudson.m.vo@gmail.com>
2026-08-04 17:48:42 -0600

    connect db and auth

---
+++ auth.ts
@@ -0,0 +1,31 @@
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
+import pool from "@/lib/db";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [
+    Credentials({
+      credentials: {
+        email: {},
+        password: {},
+      },
+      authorize: async (credentials) => {
+        const result = await pool.query(
+          "SELECT * FROM users WHERE email = $1",
+          [credentials.email]
+        );
+        const user = result.rows[0];
+        if (!user) return null;
+
+        const passwordsMatch = await bcrypt.compare(
+          credentials.password as string,
+          user.password_hash
+        );
+        if (!passwordsMatch) return null;
+
+        return { id: user.id, email: user.email, name: user.display_name };
+      },
+    }),
+  ],
+});
